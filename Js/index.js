2f6410def76836835f920ed7b0dc939c25e7098a
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2025-02-04 23:51:52 +0600

    validation complete

---
+++ Js/index.js
@@ -0,0 +1,21 @@
+// Step:1 Set button EventHandler
+document.getElementById('btn-login').addEventListener('click',function(event){
+    // Step:2 browser reloade problem slove
+    event.preventDefault()              //preventDefault() eta beboharer karon holo eta na dile page ta auto reloade howe jaw,,, console kore ar kisu pawa jaw na
+    // console.log("thanks for login us")
+
+    // Step:3 Get Phone Number
+    const phoneNumber = document.getElementById("phone");
+    const phoneValue = phoneNumber.value;
+    const pin = document.getElementById('pin');
+    const pinValue = pin.value;
+    // console.log(phoneValue , pinValue)
+
+    // Step:4 Phone and pin validation Cheek (this is not actual right way)
+    if(phoneValue === '5' && pinValue === '6721'){
+        console.log("you are logedin")
+    }
+    else{
+        alert("wrong pin or phone")
+    }
+})
